d0452655b25798301de24b9388e79343b8316872
avneet1604 <avneet.devgan@gmail.com>
2021-07-05 21:54:34 +0530

    created final spectagram

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDDKtKmzz6RRTcoE-UuXKhLwL-vAdqNUNw",
+  authDomain: "spectagram-65e98.firebaseapp.com",
+  projectId: "spectagram-65e98",
+  storageBucket: "spectagram-65e98.appspot.com",
+  messagingSenderId: "300638321382",
+  appId: "1:300638321382:web:39ea200b9b317c191b1927"
+};
